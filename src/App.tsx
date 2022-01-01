9a4b3886d48c5aedfcfa7b423ce4d623bc7fc4a1
chanhong-ji <jichanhong24@gmail.com>
2022-01-01 20:35:27 +0900

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
 function App() {
-  return null;
+  return <Router></Router>;
 }
 
 export default App;
